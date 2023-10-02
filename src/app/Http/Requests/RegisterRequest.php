f83ec70c91920523a96c74ff70939c241404633e
Karl Jerico <karl@bpoc.co.jp>
2023-10-02 14:01:57 +0800

    added a confirm password and removed the method authorized

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -10,14 +10,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class RegisterRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -27,7 +19,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'min:3', 'max:10', Rule::unique('users', 'name')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:6', 'max:200'],
+            'password' => ['required', 'min:6', 'max:100', 'confirmed'],
         ];
     }
 }
