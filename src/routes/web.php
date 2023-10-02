c54cdf1cd298fc90b5f06319e3dd0f8da4ebefdd
Karl Jerico <karl@bpoc.co.jp>
2023-10-02 13:21:22 +0800

    fixed the route

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('register');
-});
-
+Route::get('/', [RegisterController::class, 'view']);
 Route::post('/register', [RegisterController::class, 'register']);
-
