115ffd03db23c4701ded2ab93bb061fadc456538
Karl Jerico <karl@bpoc.co.jp>
2023-10-02 13:18:36 +0800

    Modified the Register Controller

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
+use App\Http\Requests\RegisterRequest;
 
 class RegisterController extends Controller
 {
-    public function register(Request $request) {
-        $incomingFields = $request->validate([
-            'name' => ['required', 'min:3', 'max:10', Rule::unique('users', 'name')],
-            'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:8', 'max:200']
-        ]);
+    public function view(){
+        return view('auth/register');
+    }
 
+    public function logout() {
+        auth()->logout();
+        return redirect('/');
+    }
+    
+    public function register(RegisterRequest $request)
+    {
+        $incomingFields = $request->validated();
+    
         $incomingFields['password'] = bcrypt($incomingFields['password']);
         $user = User::create($incomingFields);
         auth()->login($user);
