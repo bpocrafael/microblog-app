310325dd0f1cf92e1b918f46b2191bd4f17d49fc
Karl Jerico <karl@bpoc.co.jp>
2023-10-03 13:16:34 +0800

    modified the routes for the connection of login and register
    modified the routes for the login auth
    
    edited the endsection for both and fixed the name class for the login UI
    
    created a login controller
    
    Fixed some minor errors
    
    Created a home controller
    
    created a initial home page
    
    removed the form request
    
    change the name of the email and password
    
    modify routes
    
    delete initial home controller
    
    delete inital home page
    
    modified login controller
    
    modified the controllers
    
    created an initial logout button
    
    modified the routes
    
    modified login controller
    
    modified login controller
    
    fix some minor errors
    
    modified for minor spacing
    
    add return type
    
    doblock is removed
    
    added return type
    
    fixed some spacing
    
    modified routes with naming convention
    
    Modified all blades and controller depends to routes
    
    modified the reg controller for routes
    
    Modified the proper routing
    
    Fixed the routing to all

--- src/app/Http/Controllers/LoginController.php
+++ src/app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
 
 class LoginController extends Controller
 {
@@ -10,4 +12,20 @@ class LoginController extends Controller
     {
         return view('auth/login');
     }
+
+    public function auth(LoginRequest $request): RedirectResponse
+    {
+        $validatedData = $request->validated();
+    
+        if (auth()->attempt([
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password']
+        ])) {
+            $request->session()->regenerate();
+            return redirect()->route('profile.show');
+        }
+    
+        return redirect()->route('view.login');
+    }
+
 }
--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -7,21 +7,20 @@ use App\Models\User;
 use Illuminate\Contracts\View\View;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
 
 class RegisterController extends Controller
 {
-    public function view(): View
+    public function create(): View
     {
         return view('auth/register');
     }
 
-    public function register(RegisterRequest $request): RedirectResponse
+    public function store(RegisterRequest $request): RedirectResponse
     {
         $incomingFields = $request->validated();
         $incomingFields['password'] = bcrypt($incomingFields['password']);
         $user = User::create($incomingFields);
         auth()->login($user);
-        return redirect('/');
+        return redirect('profile/show');
     }
 }
