55eb6bea84a1a9a27797852435d9ab986dd1272c
Shiara <shiara@bpoc.co.jp>
2023-10-03 13:16:14 +0800

    add controller for user

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+
 class UserController extends Controller
 {
-    public function profile()
+    public function profile(): View
     {
         return view('user.profile');
     }
