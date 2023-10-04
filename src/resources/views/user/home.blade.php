794a88b029836d5f4baa825aac92e2786e4778e2
Shiara <shiara@bpoc.co.jp>
2023-10-04 14:58:30 +0800

    modify card sizes

--- src/resources/views/user/home.blade.php
+++ src/resources/views/user/home.blade.php
@@ -12,10 +12,10 @@
                 <div class="card-body" style="background-color: #EAF2F8">
                     <form method="POST" action="" enctype="multipart/form-data">
                         @csrf
-                        <div class="form-group mb-2">
+                        <div class="form-group mt-2">
                             <textarea class="form-control" id="post_content" name="post_content" rows="5" maxlength="140"></textarea>
                         </div>
-                        <div class="form-group mb-2">
+                        <div class="form-group mt-3">
                             <input type="file" class="form-control-file" id="post_image" name="post_image">
                         </div>
                         @if(session('image-preview'))
@@ -24,7 +24,7 @@
                             <img src="{{ session('image-preview') }}" alt="Image Preview" class="img-thumbnail" width="200">
                         </div>
                         @endif
-                        <div class="d-grid gap-2 d-md-flex justify-content-end">
+                        <div class="d-grid gap-2 d-md-flex justify-content-end mt-2">
                             <button type="submit" class="btn text-white" style="background-color: #023047">Create Post</button>
                         </div>
                     </form>
@@ -37,7 +37,7 @@
                 <div class="card-header" style="background-color: #2190AE">
                     <h5 class="my-2 text-white">Users you may know</h5>
                 </div>
-                <li class="list-group-item my-1" style="background-color: #EBF5FB">
+                <li class="list-group-item my-2" style="background-color: #EBF5FB">
                     <div class="row align-items-center" style="padding-left: 10px;">
                         <div class="col-auto">
                             <img src="profile.jpg" alt="Profile Picture" width="40" height="40" class="rounded-circle">
@@ -50,7 +50,7 @@
                         </div>
                     </div>
                 </li>
-                <li class="list-group-item my-1" style="background-color: #EBF5FB">
+                <li class="list-group-item my-2" style="background-color: #EBF5FB">
                     <div class="row align-items-center" style="padding-left: 10px;">
                         <div class="col-auto">
                             <img src="profile.jpg" alt="Profile Picture" width="40" height="40" class="rounded-circle">
@@ -63,7 +63,7 @@
                         </div>
                     </div>
                 </li>
-                <li class="list-group-item my-1" style="background-color: #EBF5FB">
+                <li class="list-group-item my-2" style="background-color: #EBF5FB">
                     <div class="row align-items-center" style="padding-left: 10px;">
                         <div class="col-auto">
                             <img src="profile.jpg" alt="Profile Picture" width="40" height="40" class="rounded-circle">
@@ -76,13 +76,26 @@
                         </div>
                     </div>
                 </li>
-                <li class="list-group-item my-1" style="background-color: #EBF5FB">
+                <li class="list-group-item my-2" style="background-color: #EBF5FB">
                     <div class="row align-items-center" style="padding-left: 10px;">
                         <div class="col-auto">
                             <img src="profile.jpg" alt="Profile Picture" width="40" height="40" class="rounded-circle">
                         </div>
                         <div class="col">
                             <span class="text-black">Username 4</span>
+                        </div>
+                        <div class="col-auto" style="padding-right: 25px;">
+                            <a href="#" class="btn btn-sm btn-primary like float-right">Follow</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item my-2" style="background-color: #EBF5FB">
+                    <div class="row align-items-center" style="padding-left: 10px;">
+                        <div class="col-auto">
+                            <img src="profile.jpg" alt="Profile Picture" width="40" height="40" class="rounded-circle">
+                        </div>
+                        <div class="col">
+                            <span class="text-black">Username 5</span>
                         </div>
                         <div class="col-auto" style="padding-right: 25px;">
                             <a href="#" class="btn btn-sm btn-primary like float-right">Follow</a>
