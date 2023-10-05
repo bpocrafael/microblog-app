bb180ee0e2644c2212b2c89df7ba5464b5ec659b
karljerico <144644842+karljerico@users.noreply.github.com>
2023-10-05 13:16:56 +0800

    Feat/authentication (#10)
    * modified the routes for the connection of login and register
    
    modified the routes for the login auth
    
    edited the endsection for both and fixed the name class for the login UI
    
    created a login controller
    
    Fixed some minor errors
    
    Created a home controller
    
    created a initial home page
    
    removed the form request
    
    change the name of the email and password
    
    modify routes
    
    delete initial home controller
    
    delete inital home page
    
    modified login controller
    
    modified the controllers
    
    created an initial logout button
    
    modified the routes
    
    modified login controller
    
    modified login controller
    
    fix some minor errors
    
    modified for minor spacing
    
    add return type
    
    doblock is removed
    
    added return type
    
    fixed some spacing
    
    modified routes with naming convention
    
    Modified all blades and controller depends to routes
    
    modified the reg controller for routes
    
    Modified the proper routing
    
    Fixed the routing to all
    
    * Fixed file path for reg controller

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
 
                   <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                  <form action="/register" method="POST" class="mx-1 mx-md-1">
+                  <form action="{{ route('register.store') }}" method="POST" class="mx-1 mx-md-1">
                     @csrf
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-user fa-lg me-3 fa-fw"></i>
@@ -52,7 +52,7 @@
                     </div>
 
                   </form>
-  
+
                   </div>
                 </div>
               </div>
@@ -63,4 +63,4 @@
     </div>
   </section>
 
-@section('content')
+@endsection
