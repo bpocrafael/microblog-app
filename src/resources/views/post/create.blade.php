97d49bf4e12836528ef774166ed98c0f8d0e56f1
shiara02 <shiara@bpoc.co.jp>
2023-10-09 14:04:12 +0800

    hidden input field added for return route

--- src/resources/views/post/create.blade.php
+++ src/resources/views/post/create.blade.php
@@ -6,7 +6,8 @@
                     <h5 class="my-2 text-light">Add your post</h5>
                 </div>
                 <div class="card-body" style="background-color: #EAF2F8">
-                    <form method="POST" action="/create-post" enctype="multipart/form-data">
+                    <input type="hidden" name="return_route" value="{{ Route::currentRouteName() }}">
+                    <form method="POST" action="{{ route('post.create', ['return_route' => Route::currentRouteName()]) }}" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group mt-2">
                             <textarea class="form-control" id="content" name="content" rows="5" maxlength="140"></textarea>
