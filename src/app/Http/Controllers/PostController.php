b4b15c3ecd889b1de6d4a225402f2184001cec09
shiara02 <shiara@bpoc.co.jp>
2023-10-09 13:57:09 +0800

    return route after creating post

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -2,38 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-        
-    }
 
-    public function create(Request $request)
+    public function create(PostRequest $request)
     {
-        $incomingFields = $request->validate([
-            'content' => 'required',
-            'image' => 'image|mimes:jpeg,png,gig|max:2048'
-        ]);
-
         $post = new Post();
-        $post->content = $incomingFields['content'];
+        $post->content = $request->input('content');
         $post->user_id = auth()->id();
 
         if($request->hasFile('image')) {
             $destination_path = 'public/images';
             $image = $request->file('image');
             $image_name = $image->getClientOriginalName();
-            $path = $image->storeAs($destination_path,$image_name);
+            $path = $image->storeAs($destination_path, $image_name);
 
             $input['image'] = $image_name;
             $post->image = $path;
         }
 
         $post->save();
-        return redirect()->route('user.home');
+        $returnRoute = $request->input('return_route', 'user.home');
+        return redirect()->route($returnRoute);
     }
 }
