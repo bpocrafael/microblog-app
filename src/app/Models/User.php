b81ec99f680bf594817af6cc45fec05332229efe
karljerico <karl@bpoc.co.jp>
2023-10-09 14:59:11 +0800

    Modified the user model for email verification

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+<<<<<<< HEAD
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Post;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+=======
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+>>>>>>> e3e8aa5 (Modified the user model for email verification)
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
