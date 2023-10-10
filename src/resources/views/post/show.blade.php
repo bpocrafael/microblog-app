6e1cbae5d4560d1dfd7335f4ca47248c7cee77fb
shiara02 <shiara@bpoc.co.jp>
2023-10-10 15:58:54 +0800

    displaying post image added

--- src/resources/views/post/show.blade.php
+++ src/resources/views/post/show.blade.php
@@ -1,14 +1,22 @@
-<ul class="list-group">
+<div class="container">
     @foreach ($posts as $post)
-        <li class="list-group-item" style="background-color: #FFFFF0">
-            <h5 class="card-title">{{ $post->user->name }}</h5>
-            <p class="card-text">{{ $post->content }}</p>
-            <img src="{{ asset(''.$post->image) }}" alt="Post Image" class="img-fluid">
-            <div class="interaction mt-4">
-                <a href="#" class="btn btn-sm btn-primary like">Like</a>
-                <a href="#" class="btn btn-sm btn-success comment">Comment</a>
-                <a href="#" class="btn btn-sm btn-danger share">Share</a>
+        <div class="card mb-3" style="background-color:#FAF9F6">
+            <div class="card-body">
+                <h4 class="card-title">{{ $post->user->name }}</h4>
+                <p class="card-text">{{ $post->content }}</p>
+                @if ($post->image)
+                    <img height="250px" width="150px" src="{{ asset('/storage/images/'.$post->image) }}" alt="Post Image" class="img-fluid">
+                @endif
             </div>
-        </li>
+            <div class="card-footer d-flex justify-content-between align-items-center">
+                <div class="interaction mt-1">
+                    <a href="#" class="btn btn-sm btn-primary like">Like</a>
+                    <a href="#" class="btn btn-sm btn-success comment">Comment</a>
+                </div>
+                <div>
+                    <a href="#" class="btn btn-sm btn-danger share">Share</a>
+                </div>
+            </div>
+        </div>
     @endforeach
-</ul>
+</div>
