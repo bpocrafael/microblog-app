be1e25ea7360e7bee2ea36eab03a5ba2e0b3cd9e
karljerico <karl@bpoc.co.jp>
2023-10-10 14:46:20 +0800

    Customized the Forgot password controller and UI

--- src/app/Http/Controllers/Auth/ForgotPasswordController.php
+++ src/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,21 +2,34 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\RedirectResponse;
 
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
-    |
-    */
-
     use SendsPasswordResetEmails;
+
+    // Show the form to request a password reset link.
+    public function showLinkRequestForm(): View
+    {
+        return view('auth.passwords.email');
+    }
+
+    // Send a reset link to the given user.
+    public function sendResetLinkEmail(Request $request): RedirectResponse
+    {
+        $this->validateEmail($request);
+
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+            ? $this->sendResetLinkResponse($request, $response)
+            : $this->sendResetLinkFailedResponse($request, $response);
+    }
 }
