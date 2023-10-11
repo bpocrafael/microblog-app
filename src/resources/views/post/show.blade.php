62cb5c088d332508f420cb73fa514ff91e47a5d3
shiara02 <shiara@bpoc.co.jp>
2023-10-11 15:03:48 +0800

    add show post for viewing the specific post

--- src/resources/views/post/show.blade.php
+++ src/resources/views/post/show.blade.php
@@ -1,9 +1,22 @@
-<div class="container">
-    @foreach ($posts as $post)
-        <div class="card mb-3" style="background-color:#FAF9F6">
+@extends('layout.app')
+
+@section('content')
+
+    @include('layout.navbar')
+
+    <div class="container mt-4">
+        <div class="card" style="background-color: #FAF9F6; border: 2px solid #FFA903;">
+            <div class="card-header d-flex justify-content-between align-items-center" style="background-color: #FFA903">
+                <div class="text-muted small">
+                    Created at: {{ $post->created_at->setTimezone('Asia/Manila')->format('j M Y \a\t g:ia') }}
+                </div>
+                <div class="text-muted small">
+                    Updated at: {{ $post->updated_at->setTimezone('Asia/Manila')->format('j M Y \a\t g:ia') }}
+                </div>
+            </div>
             <div class="card-body">
-                <h4 class="card-title">{{ $post->user->name }}</h4>
-                <p class="card-text">{{ $post->content }}</p>
+                <h3 class="card-title">{{ $post->user->name }}</h3>
+                <p class="card-text" style="font-size: 20px;">{{ $post->content }}</p>
                 @if ($post->image)
                     <img height="250px" width="150px" src="{{ asset('/storage/images/'.$post->image) }}" alt="Post Image" class="img-fluid">
                 @endif
@@ -18,5 +31,5 @@
                 </div>
             </div>
         </div>
-    @endforeach
-</div>
+    </div>
+@endsection
