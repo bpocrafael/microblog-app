33db5384f2dd2b155d876df8daff9a80e9f1c0d4
shiara02 <shiara@bpoc.co.jp>
2023-10-11 14:58:52 +0800

    rename show to index then modify the UI

---
+++ src/resources/views/post/index.blade.php
@@ -0,0 +1,29 @@
+<div class="container">
+    @foreach ($posts as $post)
+        <div class="card mb-3" style="background-color: #FAF9F6; border: 2px solid #FFA903;">
+            <div class="card-header d-flex justify-content-end" style="background-color: #FFA903">
+                <a href="{{ route('post.show', $post->id) }}" class="btn btn-light btn-sm" style="margin-right:10px">View</a>
+                <a href="{{ route('post.edit', $post->id) }}" class="btn btn-light btn-sm">Edit</a>
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">{{ $post->user->name }}</h3>
+                <p class="text-secondary small text-xs opacity-75">
+                    <i>{{ $post->updated_at->setTimezone('Asia/Manila')->format('j M Y \a\t g:ia') }}</i>
+                </p>
+                <p class="card-text" style="font-size: 20px;">{{ $post->content }}</p>
+                @if ($post->image)
+                    <img height="250px" width="150px" src="{{ asset('/storage/images/'.$post->image) }}" alt="Post Image" class="img-fluid">
+                @endif
+            </div>
+            <div class="card-footer d-flex justify-content-between align-items-center">
+                <div class="interaction mt-1">
+                    <a href="#" class="btn btn-sm btn-primary like">Like</a>
+                    <a href="#" class="btn btn-sm btn-success comment">Comment</a>
+                </div>
+                <div>
+                    <a href="#" class="btn btn-sm btn-danger share">Share</a>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
