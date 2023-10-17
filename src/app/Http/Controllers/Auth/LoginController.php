6866e9a00facb7629fa2c0f2af5c6f280c08de00
karljerico <karl@bpoc.co.jp>
2023-10-17 13:13:19 +0800

    Fixed resend email verification route if deleted

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -24,23 +24,30 @@ class LoginController extends Controller
     public function auth(LoginRequest $request): RedirectResponse
     {
         $validatedData = $request->validated();
-
         $credentials = [
             'email' => $validatedData['email'],
             'password' => $validatedData['password'],
         ];
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if (!$user) {
+            return redirect()->route('login')->with('error', 'Email does not exist. Please register or check the email address.');
+        }
+
+        if (!$user->email_verified_at) {
+            return redirect()->route('verification.notice');
+        }
+
+        if (!$user->hasVerifiedEmail()) {
+            return redirect()->route('verification.notice');
+        }
 
         if (auth()->attempt($credentials)) {
             $request->session()->regenerate();
             return redirect()->route('profile.show');
         }
 
-        $user = User::where('email', $validatedData['email'])->first();
-
-        if ($user && $user->email_verified_at) {
-            return redirect()->route('login')->with('error', 'Incorrect password. Please try again.');
-        }
-
-        return redirect()->route('login')->with('error', 'Email is not verified or does not exist.');
+        return redirect()->route('login')->with('error', 'Incorrect password. Please try again.');
     }
 }
