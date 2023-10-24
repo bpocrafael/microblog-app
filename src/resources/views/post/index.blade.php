8f8f689e2b4098cfcd7d31c341f10900cb4d92fb
shiara02 <shiara@bpoc.co.jp>
2023-10-24 09:43:22 +0800

    fix merge conflict

--- src/resources/views/post/index.blade.php
+++ src/resources/views/post/index.blade.php
@@ -31,8 +31,6 @@
                             Like
                         @endif
                     </button>
-                    <a href="#" class="btn btn-sm btn-success comment">Comment</a>
-                    <a href="#" class="btn btn-sm btn-primary like">Like</a>
                     <a href="{{ route('comments.index', $post->id) }}" class="btn btn-sm btn-success comment">Comment</a>
                 </div>
                 <div>
