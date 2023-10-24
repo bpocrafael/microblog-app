0d0718a7b3df1698e61382478b0a1a3da7822bae
karljerico <karl@bpoc.co.jp>
2023-10-24 21:04:42 +0800

    Created new model for profile info feature

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -84,4 +85,10 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(PostLike::class);
     }
+
+    public function profileInformation(): HasOne
+    {
+        return $this->hasOne(ProfileInformation::class, 'user_id');
+    }
+
 }
