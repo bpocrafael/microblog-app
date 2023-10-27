7fe78eb06298d00c257099a9104031c9a37b298d
karljerico <karl@bpoc.co.jp>
2023-10-27 15:03:59 +0800

    Added like counter JS

--- src/public/js/likeToggle.js
+++ src/public/js/likeToggle.js
@@ -1,20 +1,28 @@
-$(document).ready(function() {
-    $('.like-button').on('click', function() {
-    const post_id = $(this).data('post-id');
-    const button = $(this);
-    const csrfToken = $('meta[name="csrf-token"]').attr('content');
-    
+$(document).ready(function () {
+    $('.like-button').on('click', function () {
+        const post_id = $(this).data('post-id');
+        const button = $(this);
+        const likeCountElement = $(`.like-count[data-post-id="${post_id}"]`);
+        const initialLikes = parseInt(button.data('initial-likes'));
+        const csrfToken = $('meta[name="csrf-token"]').attr('content');
+
         $.ajax({
-                type: 'POST',
-                url: `/posts/${post_id}/like`,
-                data: {
-                    _token: csrfToken,
-                    post_id: post_id
-                },
-                success: function(response) {
-                    button.text(response.liked ? 'Unlike' : 'Like');
-                    button.data('liked', response.liked);
-                }
-            });
+            type: 'POST',
+            url: `/posts/${post_id}/like`,
+            data: {
+                _token: csrfToken,
+                post_id: post_id
+            },
+            success: function (response) {
+                button.text(response.liked ? 'Unlike' : 'Like');
+                button.data('liked', response.liked);
+
+                const updatedLikes = response.liked ? initialLikes + 1 : initialLikes - 1;
+                button.data('initial-likes', updatedLikes);
+
+                const likeText = updatedLikes === 1 ? ' Like' : ' Likes';
+                likeCountElement.text(updatedLikes + likeText).toggle(updatedLikes > 0);
+            }
         });
     });
+});
