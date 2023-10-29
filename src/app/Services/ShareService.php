c0a8273b9e62702d36483826e9afaeb8738d3d5a
shiara02 <shiara@bpoc.co.jp>
2023-10-29 15:41:06 +0800

    used guard clause and fixed redundancy

--- src/app/Services/ShareService.php
+++ src/app/Services/ShareService.php
@@ -12,7 +12,7 @@ class ShareService
      */
     public function storeSharePost(ShareRequest $request): Post
     {
-        $originalPost = Post::find((int)$request->input('original_post_id'));
+        $originalPost = $this->getOriginalPostById((int) $request->input('original_post_id'));
 
         while ($originalPost && $originalPost->original_post_id) {
             $originalPost = Post::find($originalPost->original_post_id);
@@ -22,10 +22,10 @@ class ShareService
         $post->content = $request->input('content');
         $post->user_id = (int) auth()->id();
 
-        if ($originalPost instanceof Post) {
-            $post->original_post_id = $originalPost->id;
-        } else {
+        if (!$originalPost instanceof Post) {
             $post->original_post_id = null;
+        } else {
+            $post->original_post_id = $originalPost->id;
         }
 
         $post->save();
@@ -37,14 +37,18 @@ class ShareService
      */
     public function getOriginalPost(Post $post): ?Post
     {
-        if ($post->original_post_id) {
-            $originalPost = Post::find($post->original_post_id);
-
-            if ($originalPost) {
-                return $originalPost;
-            }
+        if (!$post->original_post_id) {
+            return null;
         }
 
-        return null;
+        return $this->getOriginalPostById($post->original_post_id);
+    }
+
+    /**
+     * Retrieve the original post by ID.
+     */
+    private function getOriginalPostById(int $originalPostId): ?Post
+    {
+        return Post::find($originalPostId);
     }
 }
