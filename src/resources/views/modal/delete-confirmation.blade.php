a90dc2533c5e14d17b22a0e35ddcf1d591e542a7
karljerico <karl@bpoc.co.jp>
2023-11-07 10:02:02 +0800

    rename the modal for multiple usage

---
+++ src/resources/views/modal/delete-confirmation.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="dModal" tabindex="-1" aria-labelledby="dModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #f6f6f2; border-bottom: 5px solid #388087;">
+                <h5 class="modal-title" style="color: #388087;">Delete Confirmation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" style="color: #388087;">
+                <p>Are you sure you want to delete this? This action cannot be undone.</p>
+                <div class="text-end">
+                    <button type="button" class="btn btn" data-bs-dismiss="modal" style="color: #388087;">Cancel</button>
+                    <button type="button" class="btn btn confirm-delete" style="background-color: #388087; color: #fff;">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
