cfa7c5c8234251769b0d81e843b072ce6a9cbc3e
shiara02 <shiara@bpoc.co.jp>
2023-11-08 13:56:08 +0800

    auth frontend edited

--- src/resources/views/auth/verify.blade.php
+++ src/resources/views/auth/verify.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="vh-100" style="background-color: #023047;">
+<section class="vh-100" style="background-color: #388087;">
     <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-md-6 col-lg-5">
-                <div class="card text-white" style="border-radius: 25px; background-color: #34495E;">
-                    <div class="card-header" style="border-radius: 20px; background: #FFA902;">{{ __('Verify Your Email Address') }}</div>
+                <div class="card text-white" style="border-radius: 15px; background-color: #f6f6f2;">
+                    <div class="card-header text-center text-uppercase" style="color: #388087; border-bottom: 3px solid #388087">{{ __('Verify Your Email Address') }}</div>
 
-                    <div class="card-body">
+                    <div class="card-body" style="color: #388087;">
                         @if (session('resent'))
                             <div class="alert alert-success" role="alert">
                                 {{ __('A new verification link has been sent to your email address.') }}
@@ -26,11 +26,13 @@
 
                         <form class="d-inline" method="GET" action="{{ route('verification.resend') }}">
                             @csrf
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <label for="email">{{ __('Enter your email') }}</label>
                                 <input type="email" name="email" id="email" class="form-control">
                             </div>
-                            <button type="submit" class="btn btn-primary">{{ __('Resend Verification Email') }}</button>
+                            <div class="text-center">
+                            <button type="submit" class="btn text-white mt-3 col-12" style="background-color: #388087;">{{ __('Resend Verification Email') }}</button>
+                            </div>
                         </form>
                     </div>
                 </div>
